98d866fdc73373d899f270a666680f7ec8a9048d
jay <hejunjie@zmit.cn>
2020-10-12 15:22:57 +0800

    fixed:增加两张基础表

--- Doctrine/ORM/Rewrite/MakeEntityRegenerate/EntityRegenerator.php
+++ Doctrine/ORM/Rewrite/MakeEntityRegenerate/EntityRegenerator.php
@@ -19,7 +19,6 @@ use PHPZlc\PHPZlc\Doctrine\ORM\RuleColumn\RuleColumn;
 use Symfony\Bundle\MakerBundle\Exception\RuntimeCommandException;
 use Symfony\Bundle\MakerBundle\FileManager;
 use Symfony\Bundle\MakerBundle\Generator;
-use Symfony\Bundle\MakerBundle\Str;
 use Symfony\Bundle\MakerBundle\Util\ClassSourceManipulator;
 
 /**
@@ -32,13 +31,15 @@ final class EntityRegenerator
     private $doctrineHelper;
     private $fileManager;
     private $generator;
+    private $entityClassGenerator;
     private $overwrite;
 
-    public function __construct(DoctrineHelper $doctrineHelper, FileManager $fileManager, Generator $generator, bool $overwrite)
+    public function __construct(DoctrineHelper $doctrineHelper, FileManager $fileManager, Generator $generator, EntityClassGenerator $entityClassGenerator, bool $overwrite)
     {
         $this->doctrineHelper = $doctrineHelper;
         $this->fileManager = $fileManager;
         $this->generator = $generator;
+        $this->entityClassGenerator = $entityClassGenerator;
         $this->overwrite = $overwrite;
     }
 
@@ -186,6 +187,7 @@ final class EntityRegenerator
                 }
             }
 
+
         }
 
         foreach ($operations as $filename => $manipulator) {
@@ -194,6 +196,8 @@ final class EntityRegenerator
                 $manipulator->getSourceCode()
             );
         }
+
+
     }
 
     private function generateClass(ClassMetadata $metadata): string
@@ -237,18 +241,10 @@ final class EntityRegenerator
             return;
         }
 
-        // duplication in MakeEntity
-        $entityClassName = Str::getShortClassName($metadata->name);
-
-        $this->generator->generateClass(
+        $this->entityClassGenerator->generateRepositoryClass(
             $metadata->customRepositoryClassName,
-            'doctrine/Repository.tpl.php',
-            [
-                'entity_full_class_name' => $metadata->name,
-                'entity_class_name' => $entityClassName,
-                'entity_alias' => strtolower($entityClassName[0]),
-                'with_password_upgrade' => false,
-            ]
+            $metadata->name,
+            false
         );
 
         $this->generator->writeChanges();
