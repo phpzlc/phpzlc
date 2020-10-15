ccc9b3c6228c12c5bc66b91d0449ed52c788cb65
jay <hejunjie@zmit.cn>
2020-10-15 10:46:02 +0800

    fixed:修复

---
+++ Doctrine/SortIdGenerator.php
@@ -0,0 +1,21 @@
+<?php
+namespace PHPZlc\PHPZlc\Doctrine;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Id\AbstractIdGenerator;
+use Ramsey\Uuid\Uuid;
+
+class SortIdGenerator extends AbstractIdGenerator
+{
+    function generate(EntityManager $em, $entity)
+    {
+        $uuid = Uuid::uuid1()->toString();
+
+        $uuid = explode('-', $uuid);
+
+        $uuid = substr($uuid[0], -4) . '1' . $uuid[3] . substr($uuid[4], -4);
+
+        return time() . '-' . $uuid  . '-' . rand(1,  100);
+    }
+    
+}
