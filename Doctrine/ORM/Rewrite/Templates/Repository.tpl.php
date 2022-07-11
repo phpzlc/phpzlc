cae2cbc5d708b4f2ce16e156f6760391ad7a9448
CJayHe <1044295598@qq.com>
2022-07-11 10:50:37 +0800

    Update Repository.tpl.php
    支持Symfony5.4

--- Doctrine/ORM/Rewrite/Templates/Repository.tpl.php
+++ Doctrine/ORM/Rewrite/Templates/Repository.tpl.php
@@ -2,39 +2,37 @@
 
 namespace <?= $namespace; ?>;
 
-use <?= $entity_full_class_name; ?>;
-use Doctrine\Persistence\ManagerRegistry;
+<?= $use_statements; ?>
 use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use PHPZlc\PHPZlc\Doctrine\ORM\Repository\AbstractServiceEntityRepository;
 use PHPZlc\PHPZlc\Doctrine\ORM\Rule\Rule;
 use PHPZlc\PHPZlc\Doctrine\ORM\Rule\Rules;
 use PHPZlc\Validate\Validate;
-<?= $with_password_upgrade ? "use Symfony\Component\Security\Core\Exception\UnsupportedUserException;\n" : '' ?>
-<?= $with_password_upgrade ? "use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;\n" : '' ?>
-<?= $with_password_upgrade ? "use Symfony\Component\Security\Core\User\UserInterface;\n" : '' ?>
 
 /**
- * @method <?= $entity_class_name; ?>|null find($id, $lockMode = null, $lockVersion = null)
- * @method <?= $entity_class_name; ?>|null findOneBy(array $criteria, array $orderBy = null)
- * @method <?= $entity_class_name; ?>|null    findAssoc($rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
- * @method <?= $entity_class_name; ?>|null   findLastAssoc($rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
- * @method <?= $entity_class_name; ?>|null    findAssocById($id, $rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
- * @method <?= $entity_class_name; ?>[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- * @method <?= $entity_class_name; ?>[]    findAll($rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
- * @method <?= $entity_class_name; ?>[]    findLimitAll($rows, $page = 1, $rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
- */
-class <?= $class_name; ?> extends AbstractServiceEntityRepository<?= $with_password_upgrade ? " implements PasswordUpgraderInterface\n" : "\n" ?>
+* @extends ServiceEntityRepository<<?= $entity_class_name; ?>>
+*
+* @method <?= $entity_class_name; ?>|null find($id, $lockMode = null, $lockVersion = null)
+* @method <?= $entity_class_name; ?>|null findOneBy(array $criteria, array $orderBy = null)
+* @method <?= $entity_class_name; ?>|null    findAssoc($rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
+* @method <?= $entity_class_name; ?>|null   findLastAssoc($rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
+* @method <?= $entity_class_name; ?>|null    findAssocById($id, $rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
+* @method <?= $entity_class_name; ?>[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+* @method <?= $entity_class_name; ?>[]    findAll($rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
+* @method <?= $entity_class_name; ?>[]    findLimitAll($rows, $page = 1, $rules = null, ResultSetMappingBuilder $resultSetMappingBuilder = null, $aliasChain = '')
+*/
+class <?= $class_name; ?> extends AbstractServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, <?= $entity_class_name; ?>::class);
     }
-
+    
     public function registerRules()
     {
         // TODO: Implement registerRules() method.
     }
-
+    
     public function ruleRewrite(Rule $currentRule, Rules $rules, ResultSetMappingBuilder $resultSetMappingBuilder)
     {
         // TODO: Implement ruleRewrite() method.
